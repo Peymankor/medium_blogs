277b6cc3cbdada56099e60206e80656a671a8a44
peyman <peyman.kor@uis.no>
2023-09-01 14:39:04 +0200

    word count to used later

---
+++ word_count.py
@@ -0,0 +1,34 @@
+import nbformat
+import re
+
+
+# Create a function to count the words in a Jupyter Notebook
+def word_count(notebook_path):
+    """
+    Count the words in a Jupyter Notebook.
+    """
+
+    # Read the notebook
+    with open(notebook_path) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Count the words
+    words = 0
+    for cell in nb.cells:
+        if cell.cell_type == 'markdown' or cell.cell_type == 'code':
+            words += len(re.findall(r'\w+', cell.source))
+    return words
+
+
+if __name__ == '__main__':
+    import sys
+
+    print(word_count(sys.argv[1]))
+
+import os
+os.getcwd()
+
+word_count("/home/peyman/Documents/Creator/medium_blogs/Inventory_Example/notes.ipynb")
+
+# This process can be optimized through the commnad line
+# Need to study that further
